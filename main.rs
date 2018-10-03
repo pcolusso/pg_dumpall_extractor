122e92b85e09aa182300b37b9677570bf4b0b349
Paul Colusso <paulcolusso@gmail.com>
2018-10-03 10:23:27 +1000

    added stdin support, removed progress bars for now.

--- main.rs
+++ main.rs
@@ -1,9 +1,7 @@
 extern crate clap;
-extern crate indicatif;
 
 use clap::{Arg, App};
-use indicatif::{ProgressBar, ProgressStyle};
-use std::io::{BufReader, BufRead, BufWriter, Write};
+use std::io::{self, BufReader, BufRead, BufWriter, Write};
 use std::fs::File;
 
 fn main() {
@@ -18,14 +16,15 @@ fn main() {
       .help("Name of the database to be restored."))
     .arg(Arg::with_name("inputfile")
       .value_name("SQL DUMP")
-      .required(true)
-      .index(2)
+      .required(false)
+      .short("i")
+      .long("input-file")
       .help("File to process."))
     .arg(Arg::with_name("outfile")
       .value_name("Output File")
       .short("o")
-      .required(true)
-      .long("output")
+      .required(false)
+      .index(2)
       .help("Saves output to a file."))
     .arg(Arg::with_name("rename")
       .value_name("New Database Name")
@@ -34,34 +33,30 @@ fn main() {
       .required(false)
       .help("Generate SQL to restore to a different database name."))
     .get_matches();
+ 
+  let reader: Box<BufRead> = match arguments.value_of("inputfile") {
+    None => Box::new(BufReader::new(io::stdin())),
+    Some(file_name) => Box::new(BufReader::new(File::open(file_name).expect("Unable to open the source file.")))
+  };
 
-  let in_file = File::open(arguments.value_of("inputfile").unwrap()).expect("Unable to open file for reading.");
-  let out_file = File::create(arguments.value_of("outfile").unwrap()).expect("Unable to open file for writing.");
-  let db_name = arguments.value_of("database").unwrap_or("");
+  let mut writer: Box<Write> = match arguments.value_of("outfile") {
+    None => Box::new(BufWriter::new(io::stdout())),
+    Some(file_name) => Box::new(BufWriter::new(File::create(file_name).expect("Cannot open file for writing.")))
+  };
 
-  let f_size = in_file.metadata().unwrap().len();
-
-  let reader = BufReader::new(in_file);
-  let mut writer = BufWriter::new(out_file);
-  let pb = ProgressBar::new(f_size);
-  pb.set_style(ProgressStyle::default_bar()
-        .template("{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {bytes}/{total_bytes} ({eta})")
-        .progress_chars("#>-"));
+  let db_name = arguments.value_of("database").expect("No database name specified.");
 
   let mut found_db = false;
   let mut data_read: usize = 0;
-  pb.set_position(data_read as u64);
 
   'outer: for line in reader.lines() {
     let line = line.unwrap();
 
     data_read  += line.len();
-    pb.set_position(data_read as u64);
 
     if !found_db {
       if line.contains("\\connect") && line.contains(db_name) {
         found_db = true;
-        println!("Database founnd. Commencing extraction...");
         let new_name = arguments.value_of("rename").unwrap_or(db_name);
         let connect_line = format!("\\connect {}", new_name);
         writer.write_all(connect_line.as_bytes()).expect("Could not write a line to the file.");
@@ -76,8 +71,6 @@ fn main() {
       }
     }
   }
-
-  pb.finish_with_message("Done!");
 
   if !found_db {
     panic!("Was unable to find database {}", db_name);
